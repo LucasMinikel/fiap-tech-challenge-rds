94e9c94bf7665dc55bf4ca8559813ac8b2acb91b
LucasMinikel <lucasminikel.m@gmail.com>
2024-09-30 19:47:42 -0300

    fix name secrets

--- modules/sm/main.tf
+++ modules/sm/main.tf
@@ -1,11 +1,11 @@
-resource "aws_secretsmanager_secret" "db_secrets" {
-  name = "db_secrets_rds_mysql"
+resource "aws_secretsmanager_secret" "db_secrets_rds_mysql_rds_mysql" {
+  name = "db_secrets_rds_mysql_rds_mysql"
   recovery_window_in_days = 0 
   force_overwrite_replica_secret = true
 }
 
-resource "aws_secretsmanager_secret_version" "db_secrets" {
-  secret_id = aws_secretsmanager_secret.db_secrets.id
+resource "aws_secretsmanager_secret_version" "db_secrets_rds_mysql_rds_mysql" {
+  secret_id = aws_secretsmanager_secret.db_secrets_rds_mysql.id
   secret_string = jsonencode({
     DB_HOST     = var.db_host
     DB_PORT     = var.db_port
